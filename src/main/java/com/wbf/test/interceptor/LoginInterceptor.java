5c4c739747ac33534b69e1abf8ce7da7f8ebe825
drabC-u-m <dengriu555777042@163.com>
2019-12-23 15:11:52 +0800

    拦截器注释说明

--- src/main/java/com/wbf/test/interceptor/LoginInterceptor.java
+++ src/main/java/com/wbf/test/interceptor/LoginInterceptor.java
@@ -7,12 +7,32 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+/**
+ * Spring拦截器
+ * 先在application.xml中配置要拦截的请求
+ * @author wbf
+ *
+ */
 public class LoginInterceptor extends HandlerInterceptorAdapter{
+	//不会被拦截的请求
     private static final String[] IGNORE_URL = {"/login.jsp", "/user/login"};
+    
+    /**  
+     * 在业务处理器处理请求之前被调用  
+     * 如果返回false  
+     *     从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链 
+     * 如果返回true  
+     *    执行下一个拦截器,直到所有的拦截器都执行完毕  
+     *    再执行被拦截的Controller  
+     *    然后进入拦截器链,  
+     *    从最后一个拦截器往回执行所有的postHandle()  
+     *    接着再从最后一个拦截器往回执行所有的afterCompletion()  
+     */    
     @Override
     public boolean preHandle(HttpServletRequest request,
             HttpServletResponse response, Object handler) throws Exception {
         boolean flag = false;
+        //获取请求的url
         String url = request.getRequestURL().toString();
         //不拦截上面定义的路径
         for (String str : IGNORE_URL) {
@@ -22,10 +42,12 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
             }
         }
         if (!flag) {
+        	//判断session是否过期、是否登录过
         	HttpSession session = request.getSession(false);
             if (session != null && session.getAttribute("role")!=null) {
             	return true;
             }else{
+            	//清理session、跳转到登录页面
             	 request.getSession().invalidate();
             	 response.sendRedirect(request.getContextPath()+"/login.jsp"); 
             	 return false;
@@ -34,12 +56,21 @@ public class LoginInterceptor extends HandlerInterceptorAdapter{
         return true;
     }
     
+    /** 
+     * 在业务处理器处理请求执行完成后,生成视图之前执行的动作    
+     * 可在modelAndView中加入数据，比如当前时间 
+     */ 
     @Override
     public void postHandle(HttpServletRequest request,
             HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
     }
     
+    /**  
+     * 在DispatcherServlet完全处理完请求后被调用,可用于清理资源等   
+     *   
+     * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()  
+     */ 
     @Override
     public void afterCompletion(HttpServletRequest request,
             HttpServletResponse response, Object handler, Exception ex)
